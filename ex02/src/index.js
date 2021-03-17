543e653baafe419275a5e93f1463d5bcc26adb7a
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-17 21:43:52 +0100

    true false

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+//Only change code below this line
+function myFunction(myParametar){
+   
+if (myParametar) {
+    return "The parametar is true!";
+}
+return "The parametar is false!";
+}
+ myFunction(true);
+ myFunction(false);
+ console.log("The parametar is true!");
+ console.log("The parametar is false!");
+ //Only change code above this line
+ module.exports = myFunction;
