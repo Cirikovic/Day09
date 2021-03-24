1b990d95d7938ee33d008b01e6cb39361474a430
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-24 16:13:00 +0100

    izmjena2

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -5,16 +5,16 @@ function logicalAndOperator(num){
     } 
     return "No"; 
 }
-logicalAndOperator(45);
 logicalAndOperator(0);
 logicalAndOperator(24);
+logicalAndOperator(45);
 logicalAndOperator(57);
 logicalAndOperator(100);
-console.log(logicalAndOperator(45 > 40 && 45 < 80));
-console.log(logicalAndOperator(0 >= 40 && 0 <= 80));
-console.log(logicalAndOperator(24 >= 40 && 24 <= 80));
-console.log(logicalAndOperator(57 >= 40 && 57 <= 80));
-console.log(logicalAndOperator(100 >= 40 && 100 <= 80));
+console.log(logicalAndOperator(0, "Yes" && "No"));
+console.log(logicalAndOperator(24, "Yes" && "No"));
+console.log(logicalAndOperator(45, "Yes" && "No"));
+console.log(logicalAndOperator(57, "Yes" && "No"));
+console.log(logicalAndOperator(100, "Yes" && "No"));
 
      //Only change code above this line
 module.exports = logicalAndOperator;
