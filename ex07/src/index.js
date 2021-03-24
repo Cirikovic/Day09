c925ca041760b97efb851c272f49c82bcb0651ae
Cirikovic <almir.cirikovic@yahoo.com>
2021-03-24 11:33:26 +0100

    izmjena

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,20 +1,20 @@
 //Only change code below this line
 function logicalAndOperator(num){
-    if (num <= 80 && num >= 40){
+    if (num > 40 && num < 80){
         return "Yes";
     } 
-    return "No";
+    return "No"; 
 }
+logicalAndOperator(45);
 logicalAndOperator(0);
 logicalAndOperator(24);
-logicalAndOperator(45);
 logicalAndOperator(57);
 logicalAndOperator(100);
-console.log(logicalAndOperator(0 <= 80 && 0 >= 40));
-console.log(logicalAndOperator(24 <= 80 && 24 >= 40));
-console.log(logicalAndOperator(45 <= 80 && 45 >= 40));
-console.log(logicalAndOperator(57 <= 80 && 57 >= 40));
-console.log(logicalAndOperator(100 <= 80 && 100 >= 40));
+console.log(logicalAndOperator(45 > 40 && 45 < 80));
+console.log(logicalAndOperator(0 >= 40 && 0 <= 80));
+console.log(logicalAndOperator(24 >= 40 && 24 <= 80));
+console.log(logicalAndOperator(57 >= 40 && 57 <= 80));
+console.log(logicalAndOperator(100 >= 40 && 100 <= 80));
 
      //Only change code above this line
-     module.exports = logicalAndOperator;
+module.exports = logicalAndOperator;
