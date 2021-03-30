00228a7881a4278bc0d10d417c60a5621f467776
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-30 22:41:32 +0200

    while

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,9 +1,9 @@
 function inverseWhile() {
-  var fiveNumbers = 0;
-  while (fiveNumbers <= 5) {
+  var fiveNumbers = 5;
+  while (fiveNumbers >= 0) {
     console.log(fiveNumbers);
     fiveNumbers--;
   }
 }
-console.log(inverseWhile(0));
+console.log(inverseWhile());
 module.exports = inverseWhile;
