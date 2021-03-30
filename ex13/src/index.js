03c518180430af2cfee67df1a76b10356309369d
Cirikovic <almir.cirikovic@yhoo.com>
2021-03-30 22:28:40 +0200

    cpu

---
+++ ex13/src/index.js
@@ -0,0 +1,9 @@
+function inverseWhile() {
+  var fiveNumbers = 0;
+  while (fiveNumbers <= 5) {
+    console.log(fiveNumbers);
+    fiveNumbers--;
+  }
+}
+console.log(inverseWhile(0));
+module.exports = inverseWhile;
